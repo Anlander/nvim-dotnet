1decec41c568da1fdae49eb1aaed1225f9def778
anlander <anlanderp@gmail.com>
2025-03-13 21:24:02 +0100

    cmd line

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,4 +6,10 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- Resize with arrows
+map("n", "<Up>", ":resize -2<CR>")
+map("n", "<Down>", ":resize +2<CR>")
+map("n", "<Left>", ":vertical resize -2<CR>", opts)
+map("n", "<Right>", ":vertical resize +2<CR>", opts)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
